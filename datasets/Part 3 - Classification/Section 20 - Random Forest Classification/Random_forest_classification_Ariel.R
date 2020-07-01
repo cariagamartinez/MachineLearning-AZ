7f0df663c144ae457b402af8a74c919e75fde9e7
Ariel Ernesto Cariaga Martínez <cariagamartinez@gmail.com>
2020-07-01 18:21:55 +0200

    20200701_Update

--- datasets/Part 3 - Classification/Section 20 - Random Forest Classification/Random_forest_classification_Ariel.R
+++ datasets/Part 3 - Classification/Section 20 - Random Forest Classification/Random_forest_classification_Ariel.R
@@ -1,4 +1,4 @@
-# Clasificación con Árboles de Decisión
+# Random Forest
 
 # Importar el dataset
 dataset = read.csv('Social_Network_Ads.csv')
@@ -15,15 +15,19 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 testing_set = subset(dataset, split == FALSE)
 
-# Ajustar el clasificador con el conjunto de entrenamiento.
-#install.packages("rpart")
-library(rpart)
-classifier = rpart(formula = Purchased ~ ., 
-                   data = training_set)
+# Escalado de valores
+training_set[,1:2] = scale(training_set[,1:2])
+testing_set[,1:2] = scale(testing_set[,1:2])
+
+# Ajustar el Random Forest con el conjunto de entrenamiento.
+#install.packages("randomForest")
+library(randomForest)
+classifier = randomForest(x = training_set[,-3],
+                          y = training_set$Purchased,
+                          ntree = 10)
 
 # Predicción de los resultados con el conjunto de testing
-y_pred = predict(classifier, newdata = testing_set[,-3],
-                 type = "class")
+y_pred = predict(classifier, newdata = testing_set[,-3])
 
 # Crear la matriz de confusión
 cm = table(testing_set[, 3], y_pred)
@@ -32,14 +36,14 @@ cm = table(testing_set[, 3], y_pred)
 #install.packages("ElemStatLearn")
 library(ElemStatLearn)
 set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.05)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 250)
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = "class")
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Árbol de Decisión (Conjunto de Entrenamiento)',
-     xlab = 'Edad', ylab = 'Sueldo estimado',
+     main = 'Random Forest (Conjunto de Entrenamiento)',
+     xlab = 'Edad', ylab = 'Sueldo Estimado',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
@@ -48,19 +52,16 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualización del conjunto de testing
 set = testing_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.05)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 250)
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = "class")
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Árbol de Clasificación (Conjunto de Testing)',
-     xlab = 'Edad', ylab = 'Sueldo estimado',
+     main = 'Random Forest (Conjunto de Testing)',
+     xlab = 'Edad', ylab = 'Sueldo Estimado',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Representación del árbol de clasificación
-plot(classifier)
-text(classifier)
